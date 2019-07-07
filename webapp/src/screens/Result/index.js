95e8183f6452915184fde023c5dfeddfaca4e9ee
Gabriel Nunes <gabriel@multiverso.me>
2019-07-07 13:57:08 -0300

    Add last change

--- webapp/src/screens/Result/index.js
+++ webapp/src/screens/Result/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import Divider from '@material-ui/core/Divider';
 import ReactSwipe from 'react-swipe';
 
 
