3492a21d625e71693e362f9aa3fd2bfd328eb5f0
Gabriel Nunes <gabriel@multiverso.me>
2019-07-07 13:54:39 -0300

    Add redirect

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import Profile from './screens/Profile'
 import Find from './screens/Find'
 import Result from './screens/Result'
@@ -8,7 +8,7 @@ export default  function App() {
   return (
     <Router>
       <div>
-        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/" component={() => <Redirect to="/find" />} />
         <Route path="/profile" component={Profile} />
         <Route path="/find" component={Find} />
         <Route path="/result" component={Result} />
